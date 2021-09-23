2baaf8082b9be5bb7a6b32024d12583f07076a93
Manh Ly <lymanh.uet@gmail.com>
2021-09-23 14:29:47 +0700

    Update View Index.html

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,17 @@
 import * as Router from 'koa-router'
-const bodyParser = require('koa-bodyparser')
 import FacebookController from './controller/facebookController'
+const bodyParser = require('koa-bodyparser')
+const path = require('path')
+import { createReadStream } from 'fs'
 
 export const router = new Router()
 
 router.use([bodyParser()])
 
-router.get('/', ctx => (ctx.body = 'PASSED'))
+router.get('/', ctx => {
+  ctx.type = 'html'
+  ctx.body = createReadStream('./src/index.html')
+})
+
 router.get('/addTkqc', FacebookController.addTKQC)
 router.get('/sharePixel/:idTkqc', FacebookController.sharePixel)
