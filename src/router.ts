2d2af23db298cd10c76ce0cbb2f9579caf6166dc
Manh Ly <lymanh.uet@gmail.com>
2021-09-23 16:42:48 +0700

    update basic auth

--- src/router.ts
+++ src/router.ts
@@ -2,10 +2,11 @@ import * as Router from 'koa-router'
 import FacebookController from './controller/facebookController'
 const bodyParser = require('koa-bodyparser')
 import { createReadStream } from 'fs'
+import { basicAuth } from './middleware/auth'
 
 export const router = new Router()
 
-router.use([bodyParser()])
+router.use([basicAuth(process.env.USER_NAME, process.env.USER_PASS), bodyParser()])
 
 router.get('/', ctx => {
   ctx.type = 'html'
