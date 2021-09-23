d9f98b622a473c01241eff3a5040dc63783cef6b
Manh Ly <lymanh.uet@gmail.com>
2021-09-23 10:37:37 +0700

    update Facebook Controller

--- src/controller/facebookController.ts
+++ src/controller/facebookController.ts
@@ -4,27 +4,29 @@ import { Context } from 'koa'
 import { fbRequest } from '.'
 
 export default class FacebookController {
-  static addTKQC(ctx: Context) {
-    fbRequest
-      .post(
+  static async addTKQC(ctx: Context) {
+    try {
+      const rs = await fbRequest.post(
         `/v5.0/${ID_BM}/client_ad_accounts?access_token=`,
         `access_type=AGENCY&adaccount_id=act_id_tkqc&access_token=${TOKEN_ADMIN}&permitted_roles=[]&permitted_tasks=["ADVERTISE","ANALYZE","DRAFT","MANAGE"]`
       )
-      .then(rs => (ctx.body = rs.data))
-      .catch(err => {
-        ctx.status = err.response.status
-        ctx.body = err
-      })
+
+      ctx.body = rs.data
+    } catch (error) {
+      ctx.status = error.response.status
+      ctx.body = error.message
+    }
   }
 
-  static sharePixel(ctx: Context) {
-    const idTkqc = ctx.params
-    ctx.body = fbRequest
-      .post(`/v8.0/${ID_PIXEL}/shared_accounts`, `account_id=${idTkqc}&access_token=${TOKEN_ADMIN}&business=${ID_BM}`)
-      .then(rs => (ctx.body = rs.data))
-      .catch(err => {
-        ctx.status = err.response.status
-        ctx.body = err
-      })
+  static async sharePixel(ctx: Context) {
+    try {
+      const idTkqc = ctx.params
+      const rs = await fbRequest.post(`/v8.0/${ID_PIXEL}/shared_accounts`, `account_id=${idTkqc}&access_token=${TOKEN_ADMIN}&business=${ID_BM}`)
+
+      ctx.body = rs.data
+    } catch (error) {
+      ctx.status = error.response.status
+      ctx.body = error.message
+    }
   }
 }
