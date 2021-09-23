90b55c8c62176f34d703d4176bb3cc76c150348c
Manh Ly <lymanh.uet@gmail.com>
2021-09-23 15:14:04 +0700

    upadate env

--- src/controller/facebookController.ts
+++ src/controller/facebookController.ts
@@ -1,5 +1,3 @@
-import { ID_BM, ID_PIXEL, TOKEN_ADMIN } from '../config'
-import axios from 'axios'
 import { Context } from 'koa'
 import { fbRequest } from '.'
 
@@ -7,8 +5,8 @@ export default class FacebookController {
   static async addTKQC(ctx: Context) {
     try {
       const rs = await fbRequest.post(
-        `/v5.0/${ID_BM}/client_ad_accounts?access_token=`,
-        `access_type=AGENCY&adaccount_id=act_id_tkqc&access_token=${TOKEN_ADMIN}&permitted_roles=[]&permitted_tasks=["ADVERTISE","ANALYZE","DRAFT","MANAGE"]`
+        `/v5.0/${process.env.ID_BM}/client_ad_accounts?access_token=`,
+        `access_type=AGENCY&adaccount_id=act_id_tkqc&access_token=${process.env.TOKEN_ADMIN}&permitted_roles=[]&permitted_tasks=["ADVERTISE","ANALYZE","DRAFT","MANAGE"]`
       )
 
       ctx.body = rs.data
@@ -21,7 +19,7 @@ export default class FacebookController {
   static async sharePixel(ctx: Context) {
     try {
       const idTkqc = ctx.params
-      const rs = await fbRequest.post(`/v8.0/${ID_PIXEL}/shared_accounts`, `account_id=${idTkqc}&access_token=${TOKEN_ADMIN}&business=${ID_BM}`)
+      const rs = await fbRequest.post(`/v8.0/${process.env.ID_PIXEL}/shared_accounts`, `account_id=${idTkqc}&access_token=${process.env.TOKEN_ADMIN}&business=${process.env.ID_BM}`)
 
       ctx.body = rs.data
     } catch (error) {
--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,6 @@
 import * as Router from 'koa-router'
 import FacebookController from './controller/facebookController'
 const bodyParser = require('koa-bodyparser')
-const path = require('path')
 import { createReadStream } from 'fs'
 
 export const router = new Router()
